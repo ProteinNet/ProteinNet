9e60315a7c1792585c96f6178e13aefc3be7795f
fuofwi <qdertdmnstrndm@snu.ac.kr>
2022-10-16 17:58:17 +0900

    init.py added to preprocess folder

--- ProteinNet/utils/__init__.py
+++ ProteinNet/utils/__init__.py
@@ -1,5 +1,7 @@
-from utils.seed import set_seed
-from ProteinNet.ProteinNet.preprocess.graph_util import dense_to_sparse, to_dense_adj
+#from utils.seed import set_seed
+from ProteinNet.ProteinNet.utils.seed import set_seed
+from ProteinNet.ProteinNet.preprocess.pyg_util import dense_to_sparse, to_dense_adj
+#from ..preprocess.pyg_util import dense_to_sparse, to_ense_adj
 from utils.features import get_atom_feature_dims, get_bond_feature_dims
 from utils.nt_xent import NTXentLoss
 from utils.pooling import TopKPooling
